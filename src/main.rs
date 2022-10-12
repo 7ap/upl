c6313c68451193098f6c335f2ca52b57c7bdfb2a
7ap <109433659+7ap@users.noreply.github.com>
2022-10-12 13:27:19 -0500

    feat: add basic clap implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[arg(short, long)]
+    file: PathBuf,
+}
+
 fn main() {
+    let args = Cli::parse();
+
     println!("Hello, world!");
 }

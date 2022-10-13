364ec9657e7048fe750c8e18c9e3009215102428
7ap <109433659+7ap@users.noreply.github.com>
2022-10-12 20:17:27 -0500

    feat: add catbox uploader
    (yay!)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::path::PathBuf;
 
 use clap::Parser;
+use reqwest::blocking::multipart::Form;
+use reqwest::blocking::Client;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -10,8 +12,20 @@ struct Cli {
     file: PathBuf,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
+    let client = Client::new();
 
-    println!("Hello, world!");
+    let form = Form::new()
+        .text("reqtype", "fileupload")
+        .file("fileToUpload", args.file.as_os_str())?;
+
+    let response = client
+        .post("https://catbox.moe/user/api.php")
+        .multipart(form)
+        .send()?;
+
+    println!("URL: {:#?}", response.text().unwrap());
+
+    Ok(())
 }

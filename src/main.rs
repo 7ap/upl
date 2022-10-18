c9703881551ae1bc7fd7ca262f66e5b55d3b6c2c
7ap <109433659+7ap@users.noreply.github.com>
2022-10-17 21:43:21 -0500

    feat: add clap parser & stuff

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Path to file
+    file: PathBuf,
+
+    /// Configuration file (.toml or .sxcu)
+    config: PathBuf,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let file = args.file;
+    let config = args.config;
 
     Ok(())
 }
